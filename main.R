bdbab5648b83bc47c08bada232e5e82c0260f7dd
holiday <zx12as3420@gmail.com>
2024-07-23 09:12:18 +0800

    Update main.R

--- main.R
+++ main.R
@@ -2,18 +2,18 @@ library(dplyr)
 library(survival)
 library(survminer)
 
-clinical <- read.delim("~/Documents/2023_work/2023_lab_course/ym/survival/clinical.cart.2024-07-22/clinical.tsv")
+clinical <- read.delim("./survival/clinical.cart.2024-07-22/clinical.tsv")
 x <- c("case_id","case_submitter_id",'vital_status','days_to_last_follow_up')
 clinical <- clinical[,x]
 clinical <- clinical %>% distinct()
 
-connect <- read.delim("~/Documents/2023_work/2023_lab_course/ym/survival/gdc_sample_sheet.2024-07-22.tsv")
+connect <- read.delim("./survival/gdc_sample_sheet.2024-07-22.tsv")
 
 clinical_con <- merge(clinical, connect, by.x = "case_submitter_id", by.y = "Case.ID")
 
 value <- {}
 for(n in c(1:374)){
-  file <- file.path("~/Documents/2023_work/2023_lab_course/ym/survival/gdc_download_20240722_124106.302821/",clinical_con$File.ID[n],clinical_con$File.Name[n])
+  file <- file.path("./survival/gdc_download_20240722_124106.302821/",clinical_con$File.ID[n],clinical_con$File.Name[n])
   gex <- read.delim(file, header = TRUE,skip=1)[-c(1:4),]
   value[n] <- gex$fpkm_unstranded[n]
 }
